fd58cff6b0798210b8f7eb57b85d3d55b6086727
xychenbaihu@163.com <xychenbaihu@163.com>
2015-04-14 18:00:56 +0800

    add string_util.h string_util.cc

--- string/string_util.cc
+++ string/string_util.cc
@@ -1,17 +1,6 @@
-#ifndef __STRING_UTIL_H
-#define __STRING_UTIL_H
+#include "string_util.h"
 
-#include <string>
-
-class StringUtil {
-public:
-    static bool StringReplace(std::string& src, const char* sc, const char* dc, size_t slice_count=0);
-    static bool StringSplit(const std::string& src, const char* delims, std::vector<std::string> result_vec, size_t slice_count=0);
-    //static bool StringInIParse(const std::string& src, const char* delims, std::vector<std::string> result_vec, size_t slice_count=0);
-}
-
-#endif
-
+namespace util {
 
 bool StringUtil::StringReplace(std::string& src, const char* sc, const char* dc, size_t slice_count/*=0*/) 
 {
@@ -21,10 +10,16 @@ bool StringUtil::StringSplit(const std::string& src, const char* delims, std::ve
 {
 }
 
-bool StringUtil::StringSplit
+//bool StringUtil::StringSplit
+
+} // end of namespace util
 
 
-#ifdef _TEST_
+#ifdef _DEBUG_
 
+int main(int argc, char* argv[]) 
+{
+    return 0;
+}
 
 #endif
