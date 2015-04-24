61af9a3974d9e60e89796af052b6555888a1314e
xychenbaihu@163.com <xychenbaihu@163.com>
2015-04-24 20:29:50 +0800

    add msg_queue.h

--- util/msg_queue.h
+++ util/msg_queue.h
@@ -12,52 +12,52 @@ template <typename MTYPE, typename MUTEX=util::NullMutex >
 class MsgQueue {
 private:
     std::deque<MTYPE* > queue_;
-	MUTEX               lock_;
+    MUTEX               lock_;
 
 public:
-	MsgQueue() {}
-
-	~MsgQueue() {
-		util::Guard<MUTEX> guard(lock_);
-		while(queue_.size() > 0) {
-			MTYPE* pvalue = queue_.front();
-			if (pvalue != NULL) {
-				delete pvalue;
-				queue_.pop_front();
-			} else {
-				break;
-			}
-		}
-	}
-
-	bool post_msg(MTYPE* msg) {
-		util::Guard<MUTEX> guard(lock_);
-		if(queue_.size() > g_maxqueuesize) {
-			return false;
-		}
-		queue_.push_back(msg);
-		return true;
-	}
-
-	MTYPE* get_msg() {
-		util::Guard<MUTEX> guard(lock_);
-		
-		if(queue_.size() <= 0)
-			return NULL;
-		
-		MTYPE* pvalue = queue_.front();
-		if(pvalue != NULL) {
-			queue_.pop_front();
-			return pvalue;
-		} else {
-			return NULL;
-		}	
-	}
-
-	int size(){
-		util::Guard<MUTEX> guard(lock_);
-		return queue_.size();
-	}
+    MsgQueue() {}
+    
+    ~MsgQueue() {
+    	util::Guard<MUTEX> guard(lock_);
+    	while(queue_.size() > 0) {
+    		MTYPE* pvalue = queue_.front();
+    		if (pvalue != NULL) {
+    			delete pvalue;
+    			queue_.pop_front();
+    		} else {
+    			break;
+    		}
+    	}
+    }
+    
+    bool post_msg(MTYPE* msg) {
+    	util::Guard<MUTEX> guard(lock_);
+    	if(queue_.size() > g_maxqueuesize) {
+    		return false;
+    	}
+    	queue_.push_back(msg);
+    	return true;
+    }
+    
+    MTYPE* get_msg() {
+    	util::Guard<MUTEX> guard(lock_);
+    	
+    	if(queue_.size() <= 0)
+    		return NULL;
+    	
+    	MTYPE* pvalue = queue_.front();
+    	if(pvalue != NULL) {
+    		queue_.pop_front();
+    		return pvalue;
+    	} else {
+    		return NULL;
+    	}	
+    }
+    
+    int size(){
+    	util::Guard<MUTEX> guard(lock_);
+    	return queue_.size();
+    }
 };
 
 template <typename MTYPE, typename MUTEX=util::NullMutex  >
@@ -65,76 +65,76 @@ class MsgQueueCond {
 private:
     std::deque<MTYPE* > queue_;
 	
-	MUTEX lock_;
-	util::Condition<util::Mutex> cond_;
+    MUTEX lock_;
+    util::Condition<util::Mutex> cond_;
 
 public:
-	MsgQueueCond():cond_(lock_) {}
-
-	~MsgQueueCond() {	
-		util::Guard<MUTEX> guard(lock_);
-		while (queue_.size() > 0) {
-			MTYPE* pvalue = queue_.front();
-			if(pvalue != NULL) {
-				delete pvalue;
-				queue_.pop_front();
-			} else {
-				break;
-			}
-		}
-	}
-
-	bool post_msg(MTYPE* msg) {
-		util::Guard<MUTEX> guard(lock_);	
-        if (queue_.size() >(size_t) g_maxqueuesize) {
-			//printf("full [%d]\n",g_maxqueuesize);
-			return false;
-		}
-
-		queue_.push_back(msg);
-		cond_.signal();
-		return true;
-	}
-
-	MTYPE* get_msg() {
-		util::Guard<MUTEX> guard(lock_);
-		
-		while (queue_.size() <= 0) {
-			cond_.wait();
-		}
-		
-		MTYPE* pvalue = queue_.front();
-		if (pvalue != NULL) {
-			queue_.pop_front();
-			return pvalue;
-		} else {
-			return NULL;
-		}	
-	}
-	
-	MTYPE* get_msg(const struct timespec* time) {	
-		util::Guard<MUTEX> guard(lock_);
-		
-		if (queue_.size() <= 0) {
-			cond_.wait(time);
-			if(queue_.size() <= 0) {
-				return NULL;
-			}
-		}
-		
-		MTYPE* pvalue = queue_.front();
-		if (pvalue != NULL) {
-			queue_.pop_front();
-			return pvalue;
-		} else {
-			return NULL;
-		}	
-	}
-	
-	int size() {
-		util::Guard<MUTEX> guard(lock_);  
-		return queue_.size();
-	}
+    MsgQueueCond():cond_(lock_) {}
+    
+    ~MsgQueueCond() {	
+    	util::Guard<MUTEX> guard(lock_);
+    	while (queue_.size() > 0) {
+    		MTYPE* pvalue = queue_.front();
+    		if(pvalue != NULL) {
+    			delete pvalue;
+    			queue_.pop_front();
+    		} else {
+    			break;
+    		}
+    	}
+    }
+    
+    bool post_msg(MTYPE* msg) {
+    	util::Guard<MUTEX> guard(lock_);	
+    if (queue_.size() >(size_t) g_maxqueuesize) {
+    		//printf("full [%d]\n",g_maxqueuesize);
+    		return false;
+    	}
+    
+    	queue_.push_back(msg);
+    	cond_.signal();
+    	return true;
+    }
+    
+    MTYPE* get_msg() {
+    	util::Guard<MUTEX> guard(lock_);
+    	
+    	while (queue_.size() <= 0) {
+    		cond_.wait();
+    	}
+    	
+    	MTYPE* pvalue = queue_.front();
+    	if (pvalue != NULL) {
+    		queue_.pop_front();
+    		return pvalue;
+    	} else {
+    		return NULL;
+    	}	
+    }
+    
+    MTYPE* get_msg(const struct timespec* time) {	
+    	util::Guard<MUTEX> guard(lock_);
+    	
+    	if (queue_.size() <= 0) {
+    		cond_.wait(time);
+    		if(queue_.size() <= 0) {
+    			return NULL;
+    		}
+    	}
+    	
+    	MTYPE* pvalue = queue_.front();
+    	if (pvalue != NULL) {
+    		queue_.pop_front();
+    		return pvalue;
+    	} else {
+    		return NULL;
+    	}	
+    }
+    
+    int size() {
+    	util::Guard<MUTEX> guard(lock_);  
+    	return queue_.size();
+    }
 };
 
 }
